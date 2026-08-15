2595488edaf9271def2aa1d6a695d73ff5872219
iseiseje <iseiseje@users.noreply.github.com>
2026-08-15 17:44:30 +0700

    fix: add environment variables to eas.json preview profile and fallback constants in supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://axkgduqdqwnyvhzpkrnj.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4a2dkdXFkcXdueXZoenBrcm5qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDM4OTc0MywiZXhwIjoyMDk5OTY1NzQzfQ.KWgbzsj2xM4_S19wuJXF1JyVeuYW3EEk6xiG8ng2ZTs';
 
 console.log('SUPABASE URL BEING USED IN APP:', supabaseUrl);
 
@@ -12,7 +12,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
   console.warn('Supabase URL or Anon Key is missing. Ensure they are set in your .env file.');
 }
 
-export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '', {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
